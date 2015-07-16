3efba3438bc119d4d489506d7c72d1baf4b9d1a5
Andre Luiz Morita <andreluizmorita@gmail.com>
2015-07-16 15:49:30 -0300

    Fixed make:seed replace name class
    Fixed the error for replace the name class when use make:seed

--- src/Commands/SeedMakeCommand.php
+++ src/Commands/SeedMakeCommand.php
@@ -3,6 +3,7 @@
 namespace Laracasts\Generators\Commands;
 
 use Illuminate\Console\GeneratorCommand;
+use Illuminate\Filesystem\Filesystem;
 use Symfony\Component\Console\Input\InputOption;
 
 class SeedMakeCommand extends GeneratorCommand
@@ -58,5 +59,32 @@ class SeedMakeCommand extends GeneratorCommand
     protected function getPath($name)
     {
         return './database/seeds/' . str_replace('\\', '/', $name) . '.php';
+    }
+    
+    /**
+     * Build the class with the given name.
+     *
+     * @param  string $name
+     * @return string
+     */
+    protected function buildClass($name = null)
+    {
+        $stub = $this->files->get($this->getStub());
+
+        return $this->replaceClassName($stub, $name);
+    }
+
+    /**
+     * Apply the name of the seed table to the stub.
+     *
+     * @param  string $stub
+     * @param  string $name
+     * @return $this
+     */
+    protected function replaceClassName(&$stub, $name)
+    {
+        $stub = str_replace('{{class}}', ucwords(camel_case($name)), $stub);
+
+        return $stub;
     }
 }
